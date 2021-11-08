073a563399ee2455eb3af15467b22ba0ebb2e533
chardhm <chych1317@gmail.com>
2021-11-08 14:18:47 -0600

    DB creation and Models

--- api/src/models/Videogame.js
+++ api/src/models/Videogame.js
@@ -1,12 +1,34 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes, Sequelize } = require('sequelize');
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('videogame', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: Sequelize.UUIDV4, //puede ser v1 o v4, es para que nos dé un ID único por asi decirlo
+      primaryKey: true,
+      allowNull: false, //allowNull permite que este vacio, false significa que no me permite que este vacio
+    }, 
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    description: {
+      type: DataTypes.TEXT,
+    },
+    released: {
+      type: DataTypes.DATE,
+      allowNull: true,
+    },
+    rating: {
+      type: DataTypes.REAL,
+      allowNull: true,
+    },
+    platform: {
+      type: DataTypes.ARRAY(DataTypes.JSON),
+      allowNull: false,
+    }
   });
+  ({timestamps: false});
 };
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -2,11 +2,40 @@ const { Router } = require('express');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
+const { Videogame, Genre } = require('../db');
+
 
 const router = Router();
+
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 
 
+
 module.exports = router;
+
+
+
+
+/*
+
+//api key (quitar despues) = 931b4e802056402a9ad13bc2c9e98c48
+
+~ API ~
+
+VideoGames and Genres API links
+
+https://api.rawg.io/api/games?key={API_KEY}
+https://api.rawg.io/api/games?key=931b4e802056402a9ad13bc2c9e98c48
+https://api.rawg.io/api/games/{id}?key={API_KEY}
+https://api.rawg.io/api/games/1234?key=931b4e802056402a9ad13bc2c9e98c48
+
+https://api.rawg.io/api/genres?key={API_KEY}
+https://api.rawg.io/api/genres?key=931b4e802056402a9ad13bc2c9e98c48
+https://api.rawg.io/api/genres/{id}?key={API_KEY}
+https://api.rawg.io/api/genres/1?key=931b4e802056402a9ad13bc2c9e98c48
+
+
+
+*/
